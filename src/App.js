087d656158b8a404f8b81d14ec49abdc87b45da3
Bancila07 <sasa.bancila@gmail.com>
2023-07-05 12:22:14 -0900

    Add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
+import './App.scss'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Square({value, onSquareClick}) {
+    return (
+        <button className="board__square" onClick={onSquareClick}>
+            {value}
+        </button>
+    );
 }
 
-export default App;
+export default function Board() {
+    const [xIsNext, setIsNext] = useState(true);
+    const [squares, setSquares] = useState(Array(9).fill(null));
+    const [gameOver, setGameOver] = useState(false);
+    const [isDraw, setIsDraw] = useState(false)
+
+    const winner = calculateWinner(squares);
+    let status;
+    if (gameOver) {
+        if (isDraw) {
+            status = 'Draw';
+        } else {
+            status = 'Winner: ' + winner;
+        }
+    } else if (winner) {
+        status = 'Winner: ' + winner;
+    } else {
+        status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+    }
+
+    function handleClick(i) {
+        if (gameOver || squares[i] || calculateWinner(squares)) {
+            return;
+        }
+
+        const nextSquares = squares.slice();
+        if (xIsNext) {
+            nextSquares[i] = 'X';
+        } else {
+            nextSquares[i] = 'O';
+        }
+        setSquares(nextSquares);
+        setIsNext(!xIsNext);
+
+        if (calculateWinner(nextSquares)) {
+            setGameOver(true);
+        } else if (nextSquares.every(square => square !== null)) {
+            setIsDraw(true);
+            setGameOver(true);
+        }
+    }
+
+    function restartGame() {
+        setSquares(Array(9).fill(null));
+        setIsNext(true);
+        setGameOver(false);
+        setGameOver(false);
+        setIsDraw(false);
+    }
+
+    return (
+        <>
+            <div className="container">
+                <div className="status__box">
+                    <div className="status__X">X</div>
+                    <div className="status">{status}</div>
+                    <div className="status__O">O</div>
+                </div>
+                <div className="board">
+                    <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
+                    <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
+                    <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
+                </div>
+                <div className="board">
+                    <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
+                    <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
+                    <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
+                </div>
+                <div className="board">
+                    <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
+                    <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
+                    <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
+                </div>
+                <button className='button-19' onClick={restartGame}>Restart</button>
+            </div>
+        </>
+    );
+}
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (
+            squares[a] &&
+            squares[a] === squares[b] &&
+            squares[a] === squares[c]
+        ) {
+            return squares[a];
+        }
+    }
+    return null;
+}
